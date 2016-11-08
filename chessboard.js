0083af0b8494ca47d3e9388d27455301a9ca91e0
Your Name <name@example.com>
2016-11-08 13:18:14 +0700

    create board

--- chessboard.js
+++ chessboard.js
@@ -3,17 +3,18 @@ function chessboard() {
     var warna = [" Hitam", " Putih"]
     var pion = ["Pion"]
     var area = ["area"]
-    var papan = [
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""]
-    ]
+    var board1= [];
+    var board2 = [];
 
+    for (var i = 0; i < 8; i++) {
+        board1.push("")
+    }
+    for (var j = 0; j < 8; j++) {
+        board2.push(board1)
+    }
 
+    
 
 }
+
+console.log(chessboard())
