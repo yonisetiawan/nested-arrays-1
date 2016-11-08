90ed7f0191a6cb0ca9811f16c569be11cde566c9
Your Name <name@example.com>
2016-11-08 11:40:03 +0700

    susunan var chessboard

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,19 @@
-//write your code here
+function chessboard() {
+    var pemain = ["Benteng", "Kuda", "Peluncur", "Raja", "Panglima", "Peluncur", "Kuda", "Benteng"]
+    var warna = [" Hitam", " Putih"]
+    var pion = ["Pion"]
+    var area = ["area"]
+    var papan = [
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""]
+    ]
+
+
+
+}
