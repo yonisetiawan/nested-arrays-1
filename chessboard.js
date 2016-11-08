6be3ddcbac1432c426b324ed9066ba9fb80963be
Your Name <name@example.com>
2016-11-08 15:16:21 +0700

    complite chessboard & spreadsheet

--- chessboard.js
+++ chessboard.js
@@ -1,48 +1,22 @@
 function chessboard() {
     var pemain = ["Benteng", "Kuda", "Peluncur", "Raja", "Panglima", "Peluncur", "Kuda", "Benteng"]
     var warna = [" Hitam", " Putih"]
-    var pion = [" "]
-    var area = ["area"]
-    var board = [];
+    var pion = [" Pion"]
     var papan = [];
-
     for (var i = 0; i < 8; i++) {
-        board.push("")
+        papan.push([])
+        for (var j = 0; j < 8; j++) {
+            papan[i].push("")
+        }
     }
-    for (var j = 0; j < 8; j++) {
-        papan.push(board)
-    }
-
     for (var i = 0; i < 8; i++) {
-        if (papan[0][i] < 8) {
-            papan[0][i] = pemain[i] + warna[1]
-        }
-        if (papan[1][i] < 8) {
-            papan[1][i] = pion[0] + warna[1]
-        }
-        if (papan[2][i] < 8) {
-            papan[2][i] = area[0]
-        }
-        if (papan[3][i] < 8) {
-            papan[3][i] = area[0]
-        }
-        if (papan[4][i] < 8) {
-            papan[4][i] = area[0]
-        }
-        if (papan[5][i] < 8) {
-            papan[5][i] = area[0]
-        }
-        if (papan[6][i] < 8) {
-            papan[6][i] = pion[0] + warna[0]
-        }
-        if (papan[7][i] < 8) {
-            papan[7][i] = pemain[i] + warna[0]
-        }
-
+            papan[0][i] = pemain[i] + warna[0]
+            papan[1][i] = pion[0] + warna[0]
+            papan[6][i] = pion[0] + warna[1]
+            papan[7][i] = pemain[i] + warna[1]
     }
-
+    // return papan;
+    // console.log("++++++++++++++++++++++");
     return papan[7][0]
-
 }
-
 console.log(chessboard())
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,18 @@
 //write your code here
+var judul = ["number","name","position","points per game"]
+var isi = [[12,"Joe Schmo", "center",[14,32,7,0,23]],
+[9,"Ken Buckets","point guard",[19,0,11,22,0]],
+[31,"Harvey Key","shooting guard",[0,30,16,0,25]],
+[18,"Sally Talls","power forward",[18,29,26,31,19]],
+ [22,"Mo diBox","small forwared",[11,0,23,17,0]]]
+
+var table = [];
+table[0]=judul
+for(var i=0; i<isi.length; i++){
+    table[i+1]=isi[i]
+}
+
+console.log(table);
+console.log("====================");
+console.log(table[3][2])
+console.log(JSON.stringify(table[1][3]))
