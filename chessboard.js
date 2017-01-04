3a29926d1879b451ecb89d7b78cc36c29c9cd0d2
Syanmil <syanmil@yahoo.com>
2017-01-04 10:23:28 +0700

    createBoard function done

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,13 @@
 //write your code here
+"use strict"
+function createBoard(){
+  var board = []
+  for (var h = 0; h < 8; h++) {
+    board.push([])
+    for (var v = 0; v < 8; v++){
+      board[h].push([])
+    }
+  }
+  return board
+}
+console.log(createBoard());
