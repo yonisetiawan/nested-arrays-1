406cffbcf579b6f3e3d149c2ba1e629b5094df4e
Syanmil <syanmil@yahoo.com>
2017-01-04 10:59:49 +0700

    chessBoard done automatically

--- chessboard.js
+++ chessboard.js
@@ -5,28 +5,50 @@ function createBoard(){
   for (let h = 0; h < 8; h++) {
     board.push([])
     for (let v = 0; v < 8; v++){
-      board[h].push([v])
+      board[h].push([])
     }
   }
   return board
 }
 
-function fillBidak(){
+function chessBoard(){
   var board = createBoard()
   for (let i = 0; i < 8; i++){
     for (let j = 0; j < 8; j++){
       if (i == 1 || i == 6){
         board[i][j] = "Pion"
       }
-      if ((j == 0 || j == 7) && ( i == 7 || i == 0)){
-        board[i][j] = "Benteng"
+      if ( i == 7 || i == 0){
+        switch (j) {
+          case 0 :
+          case 7 :
+            board[i][j] = "Benteng"
+            break;
+          case 1 :
+          case 6 :
+            board[i][j] = "Kuda"
+            break;
+          case 2 :
+          case 5 :
+            board[i][j] = "Gajah"
+            break;
+          case 3 :
+            board[i][j] = "Raja"
+            break;
+          case 4 :
+            board[i][j] = "Ratu"
+            break;
+          default:
+        }
       }
-      // if ((j == 1 || j == 6) && ( i == 7 || i == 0)){
-      //   board[i][j] = "Kuda"
-      // }
+      if (i < 2){
+        board[i][j] += " Hitam"
+      } else if (i > 5){
+        board[i][j] += " Putih"
+      }
    }
  }
  return board
 }
-
-console.log(fillBidak());
+var chessBoard = chessBoard()
+console.log(chessBoard[0][7]);
