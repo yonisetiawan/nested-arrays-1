64e77a886ecdc4904dab04f3b48b216d447dbf8c
Syanmil <syanmil@yahoo.com>
2017-01-04 10:41:16 +0700

    adding pion and kuda

--- chessboard.js
+++ chessboard.js
@@ -2,12 +2,31 @@
 "use strict"
 function createBoard(){
   var board = []
-  for (var h = 0; h < 8; h++) {
+  for (let h = 0; h < 8; h++) {
     board.push([])
-    for (var v = 0; v < 8; v++){
-      board[h].push([])
+    for (let v = 0; v < 8; v++){
+      board[h].push([v])
     }
   }
   return board
 }
-console.log(createBoard());
+
+function fillBidak(){
+  var board = createBoard()
+  for (let i = 0; i < 8; i++){
+    for (let j = 0; j < 8; j++){
+      if (i == 1 || i == 6){
+        board[i][j] = "Pion"
+      }
+      if ((j == 0 || j == 7) && ( i == 7 || i == 0)){
+        board[i][j] = "Benteng"
+      }
+      // if ((j == 1 || j == 6) && ( i == 7 || i == 0)){
+      //   board[i][j] = "Kuda"
+      // }
+   }
+ }
+ return board
+}
+
+console.log(fillBidak());
